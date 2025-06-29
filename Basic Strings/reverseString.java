2f3e8692a517f80a6f9e2fec38c5c0c744e78bad
Aman Dhaka <asdhaka9050@gmail.com>
2025-06-29 17:49:31 +0530

    reverseString question

---
+++ Basic Strings/reverseString.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class reverseString{
+   
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of strings");
+        int n = sc.nextInt();
+        Vector<String> str = new Vector<String>();
+        System.out.println("enter the strings");
+        for(int i=0;i<n;i++){
+            String s = sc.nextLine();
+            str.add(s);
+
+
+        }
+        reverseString rs = new reverseString();
+        rs.reverseString((str));
+        System.out.println("reversed strings are:");
+      for(String s:str){
+        System.out.print(s);
+      }
+
+
+
+    }
+    public void reverseString(Vector<String> str){
+        int start =0; int end= str.size()-1;
+        while(start<end){
+            String temp = str.get(start);
+            str.set(start,str.get(end));
+            str.set(end,temp);
+            start++;
+            end--;
+            
+
+        }
+        return;
+    }
+}
